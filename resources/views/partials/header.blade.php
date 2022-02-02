48246ef76697fb5263eeb77bbecc7100aed6ae7e
francesca <fradeluca85@gmail.com>
2022-02-02 20:11:05 +0100

    header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,33 @@
-<header>
-    <img src="{{asset('img/dc-logo.png')}}" alt="Logo_DC">
+<header class="header">
+    <nav>
+        <ul class="up-nav">
+            <li>
+                <a href="#">Dc Power Visa</a>
+            </li>
+            <li>
+                <a href="#">Additional dc sites</a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container">        
+       
+        <div class="logo">
+            <a href="/"><img src="{{asset('img/dc-logo.png')}}" alt="Logo_DC"></a>
+        </div>
+        <nav class="main-nav">
+            <ul>
+                <li><a href="#">Characters</a></li>
+                <li ><a  class="active" href="#">Comics</a></li>
+                 <li>   <a href="#">Movies</a></li>
+                 <li>   <a href="#">TV</a></li>
+                  <li>  <a href="#">Games</a></li>
+                  <li>  <a href="#">Collectibles</a></li>
+                  <li>  <a href="#">Videos</a></li>
+                    <li><a href="#">Fans</a></li>
+                   <li> <a href="#">News</a></li>
+                   <li> <a href="#">Shop</a></li>
+            </ul>
+        </nav>
+        
+    </div>
 </header>
