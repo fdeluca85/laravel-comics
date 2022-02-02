d0c2b2d2f14d1e7d5a1f11d2b041b0b4be2fade5
francesca <fradeluca85@gmail.com>
2022-02-02 21:23:45 +0100

    sezione main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
 @section('pageContent')
 <div class="hero">
-    <img src="{{asset('img/jumbotron.jpg')}}" alt="">
+    <img src="{{asset('img/jumbotron.jpg')}}" alt="jumbotron">
 </div>
 
     <div class="container">
@@ -14,11 +14,30 @@
         <div class="cards">
             @foreach ($comics as $comic)
             <div class="card">
-                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
-                <h3>{{$comic['series']}}</h3>
+                <a href="#">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                    <h3>{{$comic['series']}}</h3>
+                </a>
             </div>  
         @endforeach
         </div>
+        <div class="center">
+            <button>
+                load more
+            </button>
+        </div>
        
+    </div>
+    <div class="links">
+        <div class="container">
+                <ul>
+                    <li><img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="icon comics-digital-comics"><a href="#"> Digital comics</a></li>
+                    <li><img src="{{asset('img/buy-comics-merchandise.png')}}" alt="icon comics-merchandise"><a href="#">DC Merchandise</a></li>
+                    <li><img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="icon comics-subscriptions"><a href="#">Subscription</a></li>
+                    <li><img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="icon comics-shop-locator"><a href="#">Comic shop Locator</a></li>
+                    <li><img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="icon dc-power-visa"><a href="#">CD Power Visa</a></li>
+                </ul>
+
+        </div>
     </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,15 +17,15 @@
         <nav class="main-nav">
             <ul>
                 <li><a href="#">Characters</a></li>
-                <li ><a  class="active" href="#">Comics</a></li>
-                 <li>   <a href="#">Movies</a></li>
-                 <li>   <a href="#">TV</a></li>
-                  <li>  <a href="#">Games</a></li>
-                  <li>  <a href="#">Collectibles</a></li>
-                  <li>  <a href="#">Videos</a></li>
-                    <li><a href="#">Fans</a></li>
-                   <li> <a href="#">News</a></li>
-                   <li> <a href="#">Shop</a></li>
+                <li><a  class="active" href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Collectibles</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">Fans</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Shop</a></li>
             </ul>
         </nav>
         
